34968459cc662b142c95c7382cad45376722665e
bilalhussain1996 <32520103+bilalhussain1996@users.noreply.github.com>
2018-05-07 16:08:25 -0400

    serving file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let static = require('node-static');
 
-let file = new static.Server();
+let file = new static.Server('./main.html');
 
 require('http').createServer(function(req,res){
     req.addListener('end',function(){
