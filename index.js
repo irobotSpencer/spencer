62ba6b8ac74048a5de6fc47d2e26f3163d89171e
bilalhussain1996 <32520103+bilalhussain1996@users.noreply.github.com>
2018-05-07 15:56:02 -0400

    More updates, much progress

---
+++ index.js
@@ -0,0 +1,9 @@
+let static = require('node-static');
+
+let file = new static.Server();
+
+require('http').createServer(function(req,res){
+    req.addListener('end',function(){
+        file.serve(req,res);
+    }).resume();
+}).listen(process.env.PORT|| 3000)
