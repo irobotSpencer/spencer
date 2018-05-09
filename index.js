946b579fe774f7474246b5eeabb63e9604446199
bilalhussain1996 <32520103+bilalhussain1996@users.noreply.github.com>
2018-05-09 13:58:37 -0400

    Index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-let static = require('node-static');
+// let static = require('node-static');
 
-let file = new static.Server('./');
+// let file = new static.Server('./');
 
-require('http').createServer(function(req,res){
-    req.addListener('end',function(){
-        file.serve(req,res);
-    }).resume();
-}).listen(process.env.PORT|| 3000)
+// require('http').createServer(function(req,res){
+//     req.addListener('end',function(){
+//         file.serve(req,res);
+//     }).resume();
+// }).listen(process.env.PORT|| 3000)
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+    });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
